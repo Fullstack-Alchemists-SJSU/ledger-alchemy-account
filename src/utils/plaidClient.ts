9e1f97af91d33c572d1e2055c130f22523a6b591
Aditya Kulkarni <kulkarniaditya1997@gmail.com>
2023-12-05 22:59:04 -0800

    Deployment modifications

--- src/utils/plaidClient.ts
+++ src/utils/plaidClient.ts
@@ -1,15 +1,15 @@
-import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
+import {Configuration, PlaidApi, PlaidEnvironments} from 'plaid'
 
 const configuration = new Configuration({
-    basePath: PlaidEnvironments.development, // or .development or .production
-    baseOptions: {
-        headers: {
-            'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
-            'PLAID-SECRET': process.env.PLAID_SECRET,
-        },
-    },
-});
+	basePath: PlaidEnvironments.sandbox, // or .development or .production
+	baseOptions: {
+		headers: {
+			'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
+			'PLAID-SECRET': process.env.PLAID_SECRET,
+		},
+	},
+})
 
-const plaidClient = new PlaidApi(configuration);
+const plaidClient = new PlaidApi(configuration)
 
-export default plaidClient;
+export default plaidClient
